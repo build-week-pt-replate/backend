2fe3bf40d6f1beef04a2e16c4e9c05712118c20f
cferrell2 <cferrell12182@gmail.com>
2019-04-24 23:27:45 -0400

    create seed data file for volunteers table

--- data/seeds/003-volunteers.js
+++ data/seeds/003-volunteers.js
@@ -1,13 +1,48 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  // Inserts seed entries
+  return knex("volunteers").insert([
+    {
+      id: 1,
+      username: "jsmith",
+      password: "abcd",
+      phone: "4234442222",
+      email: "jsmith@example.com",
+      firstname: "john",
+      lastname: "smith",
+      street: "123 oakwood drive",
+      city: "knoxville",
+      state: "tn",
+      zip: "37902",
+      dashboard: "???",
+      available: true
+    },
+    {
+      id: 2,
+      username: "jrambo",
+      password: "efgh",
+      phone: "4234445555",
+      email: "jrambo@example.com",
+      firstname: "john",
+      lastname: "rambo",
+      street: "321 zane lane",
+      city: "oak ridge",
+      state: "tn",
+      zip: "37748",
+      dashboard: "???",
+      available: true
+    }
+  ]);
 };
+
+// .string("username", 60)
+// tbl.string("password", 100).notNullable();
+// .string("phone", 20)
+// tbl.string("email", 60);
+// tbl.string("firstname", 60).notNullable();
+// tbl.string("lastname", 100).notNullable();
+// tbl.string("street", 160).notNullable();
+// tbl.string("city", 100).notNullable();
+// tbl.string("state", 60).notNullable();
+// tbl.string("zip", 20).notNullable();
+// tbl.string("dashboard", 100).notNullable();
+// tbl.boolean("available");
