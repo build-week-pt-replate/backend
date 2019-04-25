9a372c508968bc0c865458591ecc907e5e3d3bbe
cferrell2 <cferrell12182@gmail.com>
2019-04-24 23:42:05 -0400

    create seed data file for requests table

--- data/seeds/003-volunteers.js
+++ data/seeds/003-volunteers.js
@@ -33,16 +33,3 @@ exports.seed = function(knex, Promise) {
     }
   ]);
 };
-
-// .string("username", 60)
-// tbl.string("password", 100).notNullable();
-// .string("phone", 20)
-// tbl.string("email", 60);
-// tbl.string("firstname", 60).notNullable();
-// tbl.string("lastname", 100).notNullable();
-// tbl.string("street", 160).notNullable();
-// tbl.string("city", 100).notNullable();
-// tbl.string("state", 60).notNullable();
-// tbl.string("zip", 20).notNullable();
-// tbl.string("dashboard", 100).notNullable();
-// tbl.boolean("available");
--- data/seeds/004-requests.js
+++ data/seeds/004-requests.js
@@ -1,13 +1,33 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  // Inserts seed entries
+  return knex("table_name").insert([
+    {
+      id: 1,
+      request_date: "2019-04-24",
+      request_time: "11:00:00",
+      location_name: "Hunger First",
+      location_street: "111 Kenwood Rd",
+      location_city: "Knoxville",
+      location_state: "TN",
+      location_zip: "37902",
+      food_description: "Various canned food items",
+      comment: "Can be picked up anytime before 2pm",
+      business_id: 1,
+      volunteer_id: null
+    },
+    {
+      id: 2,
+      request_date: "2019-04-27",
+      request_time: "10:30:00",
+      location_name: "Hope Shelter",
+      location_street: "110 Carolina Street",
+      location_city: "Knoxville",
+      location_state: "TN",
+      location_zip: "37902",
+      food_description: "Boxed and canned items",
+      comment: "",
+      business_id: 2,
+      volunteer_id: 1
+    }
+  ]);
 };
