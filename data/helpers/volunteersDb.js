9b4e55041286e86a48eefdcc4af3ce0fae959935
cferrell2 <cferrell12182@gmail.com>
2019-04-25 21:55:39 -0400

    Create and export helper functions for the volunteers table.

---
+++ data/helpers/volunteersDb.js
@@ -0,0 +1,38 @@
+//* import knex/database connection
+const replateDb = require("../dbConfig");
+
+//* export volunteers table helper functions
+module.exports = {
+  getVolunteers: function() {
+    return replateDb("volunteers"); //* return array of business objects
+  },
+
+  getVolunteerById: function(id) {
+    return replateDb("volunteers")
+      .where("id", id) //* returns business within array
+      .first();
+  },
+
+  addVolunteer: function(volunteer) {
+    return replateDb("volunteers")
+      .insert(volunteer) //* returns id within array
+      .then(([id]) => {
+        return this.getVolunteerById(id);
+      });
+  },
+
+  updateVolunteer: function(id, volunteer) {
+    return replateDb("volunteers")
+      .update(volunteer)
+      .where("id", id) //* returns count of updated
+      .then(c => {
+        return this.getVolunteerById(id);
+      });
+  },
+
+  deleteVolunteer: function(id) {
+    return replateDb("volunteers")
+      .where("id", id)
+      .del(); //* returns count of deleted
+  }
+};
