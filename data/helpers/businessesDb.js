e59c3109444392a0ff80c811b15fc75ee78348e5
cferrell2 <cferrell12182@gmail.com>
2019-04-25 21:51:50 -0400

    Create helpers folder within data folder that contains the knex helper functions.  Create and export helper functions for the businesses table.

---
+++ data/helpers/businessesDb.js
@@ -0,0 +1,38 @@
+//* import knex/database connection
+const replateDb = require("../dbConfig");
+
+//* export businesses table helper functions
+module.exports = {
+  getBusinesses: function() {
+    return replateDb("businesses"); //* return array of business objects
+  },
+
+  getBusinessById: function(id) {
+    return replateDb("businesses")
+      .where("id", id) //* returns business within array
+      .first();
+  },
+
+  addBusiness: function(business) {
+    return replateDb("businesses")
+      .insert(business) //* returns id within array
+      .then(([id]) => {
+        return this.getBusinessById(id);
+      });
+  },
+
+  updateBusiness: function(id, business) {
+    return replateDb("businesses")
+      .update(business)
+      .where("id", id) //* returns count of updated
+      .then(c => {
+        return this.getBusinessById(id);
+      });
+  },
+
+  deleteBusiness: function(id) {
+    return replateDb("businesses")
+      .where("id", id)
+      .del(); //* returns count of deleted
+  }
+};
