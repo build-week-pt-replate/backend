2f42238c60f04d8e36bf57f9d78143d9bd71f62d
cferrell2 <cferrell12182@gmail.com>
2019-04-29 23:12:55 -0400

    Modify the volunteers migration file so that table fields matches what is required by client.

--- data/migrations/20190424214604_volunteers.js
+++ data/migrations/20190424214604_volunteers.js
@@ -1,8 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("volunteers", tbl => {
     tbl.increments();
+    tbl.string("firstname", 60).notNullable();
+    tbl.string("lastname", 100).notNullable();
     tbl
-      .string("username", 60)
+      .string("email", 60)
       .notNullable()
       .unique();
     tbl.string("password", 100).notNullable();
@@ -10,15 +12,9 @@ exports.up = function(knex, Promise) {
       .string("phone", 20)
       .notNullable()
       .unique();
-    tbl.string("email", 60);
-    tbl.string("firstname", 60).notNullable();
-    tbl.string("lastname", 100).notNullable();
-    tbl.string("street", 160).notNullable();
     tbl.string("city", 100).notNullable();
     tbl.string("state", 60).notNullable();
     tbl.string("zip", 20).notNullable();
-    tbl.string("dashboard", 100).notNullable();
-    tbl.boolean("available");
     tbl
       .timestamp("created_at")
       .notNullable()
