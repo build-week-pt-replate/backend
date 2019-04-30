ae41fcb96a474d4b6a181c8933cc20ff298c513f
cferrell2 <cferrell12182@gmail.com>
2019-04-29 23:09:11 -0400

    Modify the businesses migration file so that table fields matches what is required by client.

--- data/migrations/20190424214555_businesses.js
+++ data/migrations/20190424214555_businesses.js
@@ -1,25 +1,25 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("businesses", tbl => {
     tbl.increments();
-    tbl
-      .string("username", 60)
-      .notNullable()
-      .unique();
     tbl.string("password", 100).notNullable();
     tbl
       .string("phone", 20)
       .notNullable()
       .unique();
-    tbl.string("email", 60);
     tbl
-      .string("companyname", 120)
+      .string("email", 60)
       .notNullable()
       .unique();
+    tbl
+      .string("companyName", 120)
+      .notNullable()
+      .unique();
+    tbl.string("officeName", 120);
+    tbl.string("officeEmail", 60);
     tbl.string("street", 160).notNullable();
     tbl.string("city", 100).notNullable();
     tbl.string("state", 60).notNullable();
     tbl.string("zip", 20).notNullable();
-    tbl.string("dashboard", 100).notNullable();
     tbl
       .timestamp("created_at")
       .notNullable()
